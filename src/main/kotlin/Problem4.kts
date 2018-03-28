6d06e25580186f2e40bc4edf76c88d43bbbb72fd
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 23:22:47 +0200

    Problem 4

---
+++ src/main/kotlin/Problem4.kts
@@ -0,0 +1,16 @@
+
+var highest = -1
+for(x in 100 until 1000) {
+    for(y in 100 until 1000) {
+        val sum = x * y
+        if(sum.isPalindrome() && sum > highest)
+            highest = sum
+    }
+}
+
+println(highest)
+
+
+fun Int.isPalindrome() : Boolean {
+    return this.toString() == this.toString().reversed()
+}

75ab42b8592d2d0a88a5a22e175ca20b60adc0f1
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 23:31:25 +0200

    Problem 5

---
+++ src/main/kotlin/Problem5.kts
@@ -0,0 +1,17 @@
+val target = 2520
+val divisionMax = 20
+var index = 0
+
+while(!index.isEvenlyDivisibleBy(divisionMax))
+    index++
+
+println("Answer: $index")
+fun Int.isEvenlyDivisibleBy(max: Int) : Boolean {
+    if(this < max) return false
+
+    for(i in 1..max) {
+        if(this % i != 0)
+            return false
+    }
+    return true
+}

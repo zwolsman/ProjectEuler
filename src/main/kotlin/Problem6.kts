87af89703b70c620b992e8df3c8bc19887f5a295
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 23:52:54 +0200

    Problem 6

---
+++ src/main/kotlin/Problem6.kts
@@ -0,0 +1,9 @@
+import java.lang.Math.pow
+import kotlin.math.abs
+
+val target = 100
+val sumSquare = (0..target).sumBy { it * it }.toDouble()
+val squareSum = pow((0..target).sum().toDouble(), 2.0)
+val diff = abs(sumSquare - squareSum).toInt()
+
+println("diff: $diff")

92b6fbe5589ed60207e7897432bbe82fd6f628ee
Marvin <marvin.zwolsman@gmail.com>
2018-03-28 23:16:58 +0200

    Problem 3

---
+++ src/main/kotlin/Problem3.kts
@@ -0,0 +1,33 @@
+val target = 600851475143
+var temp = target
+
+var answer:Long = 0
+var i:Long = 2
+while(temp > 1) {
+    if(!i.isPrime()) {
+        i++
+        continue
+    }
+
+    if(temp % i == 0.toLong()) {
+        temp /= i
+        answer = i
+        i = 2
+    } else {
+        i++
+    }
+}
+
+println("Answer: $answer")
+
+fun Long.isPrime() :Boolean {
+    if(this == 2.toLong())
+        return true
+    val zero:Long = 0
+    for(i in this - 1 downTo 2) {
+        if (this % i == zero)
+            return false
+    }
+
+    return true
+}

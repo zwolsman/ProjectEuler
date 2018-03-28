91ca555724dd4c181e3041aada1bc407d7fc2ec2
Marvin <marvin.zwolsman@gmail.com>
2018-03-29 00:22:16 +0200

    Problem 7

---
+++ src/main/kotlin/Problem7.kts
@@ -0,0 +1,30 @@
+import java.util.*
+
+val primes = mutableListOf<Long>()
+val target = 10_001
+var index:Long = 2
+println("Starting computing $target primes!")
+val dt = Date().toInstant().toEpochMilli()
+
+while(primes.size != target) {
+    if((index).isPrime())
+        primes.add(index)
+    index++
+}
+
+println("Computed $target primes..")
+println("Took ${Date().toInstant().toEpochMilli() - dt}ms")
+println("The ${target}st prime is ${primes.last()}")
+
+println(primes)
+fun Long.isPrime() :Boolean {
+    if(this == 2.toLong())
+        return true
+    val zero:Long = 0
+    for(i in this - 1 downTo 2) {
+        if (this % i == zero)
+            return false
+    }
+
+    return true
+}
